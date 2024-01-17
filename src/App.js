7143828f972c6b9abbea6d6e5d304d76aca6ae02
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-01-17 16:38:11 +0200

    Create the front end UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greetings';
+
 function App() {
-  return <h1>React App</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
+  );
 }
 
 export default App;
